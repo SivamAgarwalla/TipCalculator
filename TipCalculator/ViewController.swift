35b317e989badb1ec486fad203524aa5e143942a
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-08-13 15:32:44 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Sivam Agarwalla on 8/13/20.
+//  Copyright © 2020 Sivam Agarwalla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
