cb03bf990a78d9c3e105df0f4957240707ab52a7
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-08-13 18:38:52 -0700

    Fixed version of the tip calculator.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalPeopleSlider: UISlider!
+    
+    @IBOutlet weak var numOfPeople: UILabel!
+    @IBOutlet weak var totForPeople: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onTop(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func calculateTIp(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let numPeople = Int(totalPeopleSlider.value)
+        numOfPeople.text = "\(numPeople) People"
+        let perPersonTot = total / Double(numPeople)
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        totForPeople.text = String(format: "$%.2f", perPersonTot)
+    }
+    
 }
 
